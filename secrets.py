83857c0a24da822caaf4e75002b7134340bc00ef
daniel1973 <danieljere@ymail.com>
2024-01-30 00:18:31 -0800

    -

--- secrets.py
+++ secrets.py
@@ -1,1 +1,9 @@
+# In secrets.py
 
+import os
+
+PG_DATABASE = os.environ.get("PG_DATABASE")
+PG_HOST = os.environ.get("PG_HOST")
+PG_PASSWORD = os.environ.get("PG_PASSWORD")
+PG_USER = os.environ.get("PG_USER")
+PG_PORT = os.environ.get("PG_PORT")
