d4306950d0289115ee6cf5c14a54cbb0ed7d7438
daniel1973 <danieljere@ymail.com>
2024-01-29 23:17:40 -0800

    -

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -89,7 +89,6 @@ WSGI_APPLICATION = 'codebattle.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -116,9 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-from secrets import test
-  
-print("TEST:", test)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
